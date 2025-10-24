3c9a53fec919d85b011169aec5237031f8e5406b
Rolpon <55262166+gamer9-create@users.noreply.github.com>
2025-10-24 14:46:55 -0400

    RIGER

--- create_exe.py
+++
@@ -1,8 +0,0 @@
-import shutil
-
-dst = ".EXE"
-
-shutil.copy("cmake-build-release/BouncingPlus.exe", dst)
-shutil.copy("cmake-build-release/glfw3.dll", dst)
-shutil.copy("cmake-build-release/raylib.dll", dst)
-shutil.copytree("assets", dst+"/assets")
--- grab.py
+++
@@ -1,7 +0,0 @@
-import os
-
-s = ""
-for i in os.listdir(os.getcwd()):
-    if i.endswith(".cpp"):
-        s+=i+" "
-print(s)
