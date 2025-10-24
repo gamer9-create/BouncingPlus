c00ffbe930e39dca4b30d8907d9471b0823796d3
Rolpon <55262166+gamer9-create@users.noreply.github.com>
2025-10-24 15:18:05 -0400

    new blooms when playtime sprouts
    come along, go to sleep!
    (come along, comma-comma-long)
    you're not alone, you will see
    (come along, comma-comma-long)
    we children we have eachother
    and eachother is all we need

--- pyscripts/create_exe.py
+++ pyscripts/create_exe.py
@@ -1,8 +1,36 @@
-import shutil
+import shutil,os
+
+def clear(path):
+    for p in os.listdir(path):
+        f = path+"\\"+p
+        if os.path.isfile(f):
+            os.remove(f)
+        else:
+            clear(f)
+    os.rmdir(path)
+    
 
 dst = ".EXE"
 
-shutil.copy("cmake-build-release/BouncingPlus.exe", dst)
-shutil.copy("cmake-build-release/glfw3.dll", dst)
-shutil.copy("cmake-build-release/raylib.dll", dst)
-shutil.copytree("assets", dst+"/assets")
+f = open("version", 'r')
+version = int(f.readline())
+f.close()
+f = open("version",'w')
+f.write(str(version+1))
+f.close()
+
+os.mkdir(dst)
+
+shutil.copy("../cmake-build-release/BouncingPlus.exe", dst)
+for file in os.listdir("../cmake-build-release"):
+    if file.endswith(".dll"):
+        shutil.copy("../cmake-build-release/"+file, dst)
+shutil.copytree("../assets", dst+"/assets")
+
+shutil.make_archive("BouncingPlusVersion"+str(version), 'zip', dst)
+
+path = "BouncingPlusVersion"+str(version)+".zip"
+command = f"file2clip.exe \"{path}\""
+os.system(command)
+
+clear(str(os.path.abspath(dst)))
